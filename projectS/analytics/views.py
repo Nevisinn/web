f51d4c78b655ff86d510f60e69a4032f16840c28
Igor <igor.kryt@mail.ru>
2023-01-07 21:08:06 +0400

    Site

--- projectS/analytics/views.py
+++ projectS/analytics/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
+menu = ["Востребованность","География","Навыки","Последние вакансии"]
 
-def analytics(request):
-    return HttpResponse("Аналитика по профессии")
+def index(request):
+    posts = Analytics.objects.all()
+    return render(request,'analytics/index.html',{'posts': posts, 'menu':menu, 'title':'Главная страница'})
 
-def categories(request):
-    return HttpResponse("Востребованность профессии")
+def about(request):
+    return render(request,'analytics/about.html',{'menu':menu,'title':'О сайте'})
